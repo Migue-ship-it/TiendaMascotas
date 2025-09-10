cecd70be3f2dbd0f52aad8f97b6096e032a65b7c
Miguel Martinez <80429586+Migue-ship-it@users.noreply.github.com>
2025-09-09 19:14:05 -0500

    Update ClienteServlet.java

--- hols/src/main/java/controlador/ClienteServlet.java
+++ hols/src/main/java/controlador/ClienteServlet.java
@@ -15,20 +15,20 @@ public class ClienteServlet extends HttpServlet {
     public ClienteServlet() {
         super();
         dao = new ClienteDAO(); // DAO inicializado al crear el servlet
-
     }
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) 
             throws ServletException, IOException {
+                /* ara recuperar información de un servidor */
         String action = request.getParameter("action");
-        try {  //parámetro "action" de la URL (segun la opcion que se elija en el action "cliente.jsp"
+        try {  //parámetro "action" de la URL (segun la opcion que se elija en el action "cliente.jsp"), en caso de ser equivalente "action"  se obtienen los parametros de las lineas 51-56
             if ("delete".equalsIgnoreCase(action)) {
                 int idCliente = Integer.parseInt(request.getParameter("id"));
-                dao.delete(idCliente);
+                dao.delete(idCliente); //Se llama al objeto dao con el metodo delete para borrar el idCliente declarado en la linea 25
                 response.sendRedirect(request.getContextPath() + "/ClienteServlet?action=list"); // Redirige a la lista de clientes después del action seleccionado
             } else if ("update".equalsIgnoreCase(action)) {
                 int idCliente = Integer.parseInt(request.getParameter("id"));
-                Cliente Cliente = dao.read(idCliente);
+                Cliente Cliente = dao.read(idCliente); 
                 request.setAttribute("Clientes", Cliente); // Envía el objeto a la vista
                 request.getRequestDispatcher("/vista/cliente.jsp").forward(request, response);
             } else if ("list".equalsIgnoreCase(action)) {
@@ -46,8 +46,11 @@ public class ClienteServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) 
             throws ServletException, IOException {
-        String action = request.getParameter("action");
-        try {
+        /*Dentro del metodo doPost (no muestra datos existentes o para crear) declara una String y le asigna el valor del parámetro recuperado, si el valor de la action es igual a la cadena "create",
+        se obtienen los parametros de las lineas 55-60 (mismo caso para la condicional con la action update en las lineas 65-71), declara un objeto llamando al constructor de la clase Cliente para guardar la informacion de los parametros
+        declara un objeto llamando al constructor de la clase Cliente para guardar la informacion de los parametros de las lineas 51-56, guardar la informacion asignada en el DAO en el objeto */
+        String action = request.getParameter("action"); 
+        try { 
             if ("create".equalsIgnoreCase(action)) {
                 int cedula = Integer.parseInt(request.getParameter("cedula"));
                 String nombres = request.getParameter("nombres");
@@ -55,8 +58,7 @@ public class ClienteServlet extends HttpServlet {
                 String direccion = request.getParameter("direccion");
                 String telefono = request.getParameter("telefono");
                 String correo = request.getParameter("correo");
-                Cliente Cliente = new Cliente(cedula, nombres, apellidos, direccion, telefono, correo);
-                // guardar en el DAO la informacion asignada en el objeto
+                Cliente Cliente = new Cliente(cedula, nombres, apellidos, direccion, telefono, correo); 
                 dao.create(Cliente);
                 response.sendRedirect(request.getContextPath() + "/ClienteServlet?action=list");
             } else if ("update".equalsIgnoreCase(action)) {
@@ -67,7 +69,6 @@ public class ClienteServlet extends HttpServlet {
                 String direccion = request.getParameter("direccion");
                 String telefono = request.getParameter("telefono");
                 String correo = request.getParameter("correo");
-                // Crear objeto basado en un constructor de la clase Cliente con las variables asignadas
                 Cliente Cliente = new Cliente(idCliente, cedula, nombres, apellidos, direccion, telefono, correo);
                 dao.update(Cliente);
                 response.sendRedirect(request.getContextPath() + "/ClienteServlet?action=list");
@@ -76,6 +77,4 @@ public class ClienteServlet extends HttpServlet {
             throw new ServletException("Error en el servlet: " + e.getMessage());
         }
     }
-
 }
-
