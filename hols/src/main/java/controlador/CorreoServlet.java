4f8a9b120872af166038b96969f2c254e2cd6e3f
Miguel Martinez <80429586+Migue-ship-it@users.noreply.github.com>
2025-09-09 19:57:24 -0500

    Update CorreoServlet.java

--- hols/src/main/java/controlador/CorreoServlet.java
+++ hols/src/main/java/controlador/CorreoServlet.java
@@ -12,17 +12,21 @@ public class CorreoServlet extends HttpServlet {
 	@Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+/*En este servlet se declaran 3 variables: to, subject y messageText, buscando un parámetro con sus nombres respectivos en la solicitud.
+En las lineas 23-25, se usa la clase Properties para asignar la configuración SMTP (servidor local sin autenticación de 2 pasos ni contraseñas de aplicacion)
+y un puerto SMTP sin SSL (protocolo de seguridad, evitando asi la autenticacion de 2 pasos, o contraseñas de aplicacion), luego en la linea 26, 
+se inicializa un Session objeto de la API JavaMail utilizando los parámetros de configuración proporcionados en el props para enviar o recibir correos electrónicos.
+Se asigna un try/catch para el proceso de creacion y envio del mensaje del remitente (linea 29) */
         String to = request.getParameter("to");
         String subject = request.getParameter("subject");
         String messageText = request.getParameter("message");
-        // Configuración SMTP (servidor local sin autenticación de 2 pasos ni contraseñas de aplicacion)
         Properties props = new Properties();
-        props.put("mail.smtp.host", "localhost"); // tu servidor SMTP
-        props.put("mail.smtp.port", "587"); // puerto típico sin SSL
+        props.put("mail.smtp.host", "localhost");
+        props.put("mail.smtp.port", "587");
         Session session = Session.getInstance(props);
-        try { //proceso de creacion y envio del mensaje
+        try {
             Message message = new MimeMessage(session);
-            message.setFrom(new InternetAddress("miguelmartinez2864@gmail.com")); // remitente
+            message.setFrom(new InternetAddress("miguelmartinez2864@gmail.com"));
             message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(to));
             message.setSubject(subject);
             message.setText(messageText);
@@ -32,6 +36,4 @@ public class CorreoServlet extends HttpServlet {
             throw new RuntimeException(e);
         }
     }
-	
-
 }
