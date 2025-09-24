384f56ea202c72a7219982b591b5a2aae032f0a8
Miguel Martinez <80429586+Migue-ship-it@users.noreply.github.com>
2025-09-24 18:51:21 -0500

    Update CorreoServlet.java

--- hols/src/main/java/controlador/CorreoServlet.java
+++ hols/src/main/java/controlador/CorreoServlet.java
@@ -6,25 +6,27 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.*;
 import java.io.IOException;
 import java.util.Properties;
+import jakarta.mail.PasswordAuthentication;
 @WebServlet("/CorreoServlet")
 public class CorreoServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	@Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
         throws ServletException, IOException {
-		/* En este codigo se refleja el proceso para el envio y seguridad de los mensajes mediante Properties, 
-		para almacenar el servidor SMTP donde se dara la comunicacion a internet para el envio de correos. 
-		Tambien el proceso y creacion del mensaje, todo esto mediante la API JavaMail.*/
         String to = request.getParameter("to");
         String subject = request.getParameter("subject");
         String messageText = request.getParameter("message");
         // Configuración SMTP (servidor local sin autenticación de 2 pasos ni contraseñas de aplicacion)
-        Properties props = new Properties();
+        Properties props = System.getProperties();
         props.put("mail.smtp.host", "localhost"); //host o direccion del servidor SMTP (local)
-        props.put("mail.smtp.port", "587"); // puerto asignado mediante una contaseña de autenticacion en el Gmail del remitente
+        props.put("mail.smtp.port", "487"); // puerto TLS
         props.put("mail.smtp.ssl.enable", "true"); //habilita el uso del SSL y protege la comunicacion SMTP
         props.put("mail.smtp.auth", "true"); //para enviar correos se autenticara mediante 2 pasos (contar con un dispositivo movil)
-        Session session = Session.getInstance(props);
+        Session session = Session.getInstance(props, new jakarta.mail.Authenticator() {
+        	protected PasswordAuthentication getPasswordAuthentication() {
+        		return new PasswordAuthentication(to, "tfeu tlxn mzqy unzi");
+        	}
+        });
         try { //proceso de creacion y envio del mensaje
             Message message = new MimeMessage(session);
             message.setFrom(new InternetAddress("miguelmartinez2864@gmail.com")); // remitente
