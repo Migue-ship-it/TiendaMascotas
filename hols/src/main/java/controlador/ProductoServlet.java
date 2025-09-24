24c226f3acd6ed18d9838c628572728a44725aac
Miguel Martinez <80429586+Migue-ship-it@users.noreply.github.com>
2025-09-24 17:36:01 -0500

    Update ProductoServlet.java

--- hols/src/main/java/controlador/ProductoServlet.java
+++ hols/src/main/java/controlador/ProductoServlet.java
@@ -19,6 +19,9 @@ public class ProductoServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) 
             throws ServletException, IOException {
+/*Dentro de este metodo (para recuperar, borrar o mostrar información de un servidor, declara una String y le asigna el valor del parámetro recuperado,
+en caso de ser equivalente "action", en las acciones "delete" y "update", se obtiene el id del producto, se llama al objeto dao con los metodos DAO para borrar, crear o mostrar los parametros segun el id declarado
+Este se redirige a la lista de productos o producto.jsp después del action seleccionado*/
         String action = request.getParameter("action");
         try { //parámetro "action" de la URL (segun la opcion que se elija en el action "producto.jsp")
             if ("delete".equalsIgnoreCase(action)) {
@@ -45,6 +48,9 @@ public class ProductoServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) 
             throws ServletException, IOException {
+        /*Dentro del metodo doPost (no muestra datos existentes o para crear) declara una String y le asigna el valor del parámetro recuperado, si el valor de la action es igual a la cadena "create",
+        se obtienen los parametros de las lineas 57-63 (mismo caso para la condicional con la action update en las lineas 64-72), declara un objeto llamando al constructor de la clase Producto para guardar la informacion de los parametros
+        declara un objeto llamando al constructor de la clase Producto para guardar la informacion de los parametros de las lineas 65-69, guardar la informacion asignada en el DAO en el objeto */
         String action = request.getParameter("action");
         try {
             if ("create".equalsIgnoreCase(action)) {
@@ -71,5 +77,6 @@ public class ProductoServlet extends HttpServlet {
     }
 
 }
+
 
 
