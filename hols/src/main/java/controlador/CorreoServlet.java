0a871f79967ef14b89163326c706f9c8ee194273
Miguel Martinez <80429586+Migue-ship-it@users.noreply.github.com>
2025-09-24 18:05:04 -0500

    Update CorreoServlet.java
    Autenticacion SMTP necesaria para el envio de correos, verificacion de 2 pasos, seguridad gmail remitente

--- hols/src/main/java/controlador/CorreoServlet.java
+++ hols/src/main/java/controlador/CorreoServlet.java
@@ -11,28 +11,26 @@ public class CorreoServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	@Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-/*En este servlet se declaran 3 variables: to, subject y messageText, buscando un parámetro con sus nombres respectivos en la solicitud.
-En las lineas 23-25, se usa la clase Properties para asignar la configuración SMTP (servidor local sin autenticación de 2 pasos ni contraseñas de aplicacion)
-y un puerto SMTP sin SSL (protocolo de seguridad, evitando asi la autenticacion de 2 pasos, o contraseñas de aplicacion), luego en la linea 26, 
-se inicializa un Session objeto de la API JavaMail utilizando los parámetros de configuración proporcionados en el props para enviar o recibir correos electrónicos.
-Se asigna un try/catch para el proceso de creacion y envio del mensaje del remitente */
+        throws ServletException, IOException {
         String to = request.getParameter("to");
         String subject = request.getParameter("subject");
         String messageText = request.getParameter("message");
+        // Configuración SMTP (servidor local sin autenticación de 2 pasos ni contraseñas de aplicacion)
         Properties props = new Properties();
-        props.put("mail.smtp.host", "localhost");
-        props.put("mail.smtp.port", "587");
+        props.put("mail.smtp.host", "localhost"); //host o direccion del servidor SMTP (local)
+        props.put("mail.smtp.port", "587"); // puerto asignado mediante una contaseña de autenticacion en el Gmail del remitente
+        props.put("mail.smtp.ssl.enable", "true"); //habilita el uso del SSL y protege la comunicacion SMTP
+        props.put("mail.smtp.auth", "true"); //para enviar correos se autenticara mediante 2 pasos (contar con un dispositivo movil)
         Session session = Session.getInstance(props);
-        try {
+        try { //proceso de creacion y envio del mensaje
             Message message = new MimeMessage(session);
-            message.setFrom(new InternetAddress("miguelmartinez2864@gmail.com"));
+            message.setFrom(new InternetAddress("miguelmartinez2864@gmail.com")); // remitente
             message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(to));
             message.setSubject(subject);
             message.setText(messageText);
             Transport.send(message);
             response.getWriter().println("Correo enviado correctamente.");
-        } catch (MessagingException e) { //Actualmente el mensaje al ejecutar el envio del correo indica un error en los props
+        } catch (MessagingException e) {
             throw new RuntimeException(e);
         }
     }
